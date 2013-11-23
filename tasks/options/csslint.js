b93ebdcd565392746de987417e6521f383d1e4ec
kwaledesign <sam@kwaledesign.com>
2013-11-23 13:09:54 -0700

    add boilerplate config to csslint

--- tasks/options/csslint.js
+++ tasks/options/csslint.js
@@ -1,3 +1,19 @@
 module.exports = {
-
+  csslint: {
+    options: {
+      csslintrc: '.csslintrc'
+    },
+    strict: {
+      options: {
+        import: 2
+      },
+      src: ['path/to/**/*.css']
+    },
+    lax: {
+      options: {
+        import: false
+      },
+      src: ['path/to/**/*.css']
+    }
+  }
 };
