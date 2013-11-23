5d4a5bc6886899a1890337876f0a9679cf72bf51
kwaledesign <sam@kwaledesign.com>
2013-11-23 10:50:29 -0700

    add boilerplate config to banner.js

--- tasks/options/banner.js
+++ tasks/options/banner.js
@@ -1,3 +1,14 @@
 module.exports = {
-
+  usebanner: {
+    taskName: {
+      options: {
+        position: '',       // 'top' || 'bottom',
+        banner: '',         // banner text <%= templates encouraged %>,
+        linebreak: true     // true || false
+      },
+      files: {
+        src: ['']           // [ 'path/to/file.ext', 'path/to/another/*.ext' ]
+      }
+    }
+  }
 };
