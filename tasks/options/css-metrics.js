3f31b1ce8363ca59a02bc945e7f7db820ac94b05
kwaledesign <sam@kwaledesign.com>
2013-11-23 12:10:50 -0700

    add boilerplate config to css-metrics.js

--- tasks/options/css-metrics.js
+++ tasks/options/css-metrics.js
@@ -1,3 +1,14 @@
 module.exports = {
-
+  cssmetrics: {
+    dev: {
+      src: [
+        'test/*.min.css'
+      ],
+      options: {
+        quiet: false,
+        maxSelectors: 4096,
+        maxFileSize: 10240000
+      }
+    }
+  }
 };
