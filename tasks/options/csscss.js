e842d25318ca7dee96f2df73970f7873c30debdc
kwaledesign <sam@kwaledesign.com>
2013-11-23 12:23:50 -0700

    add boilerplate config to csscss.js

--- tasks/options/csscss.js
+++ tasks/options/csscss.js
@@ -1,3 +1,16 @@
 module.exports = {
-
+  csscss: {
+    options: {
+      colorize: false,
+      verbose: true,
+      outputJson: false,
+      minMatch: 2,
+      compass: true,
+      ignoreProperties: 'padding',
+      ignoreSelectors: '.rule-a'
+    },
+    dist: {
+      src: ['css/style.css', 'css/another.css']
+    }
+  }
 };
