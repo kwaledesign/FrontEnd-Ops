9651f663a2383e01d94af68c877c40b9c38a34e0
kwaledesign <sam@kwaledesign.com>
2013-11-24 12:16:39 -0700

    add boilerplate config for notify.js

--- tasks/options/notify.js
+++ tasks/options/notify.js
@@ -1,3 +1,20 @@
 module.exports = {
-
+  notify: {
+    task_name: {
+      options: {
+        // Task-specific options go here.
+      }
+    },
+    watch: {
+      options: {
+        title: 'Task Complete',  // optional
+        message: 'SASS and Uglify finished running', //required
+      }
+    },
+    server: {
+      options: {
+        message: 'Server is ready!'
+      }
+    }
+  }
 };
