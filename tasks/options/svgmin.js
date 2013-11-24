632d30fff487c300a48031266b005b2df00c51ec
kwaledesign <sam@kwaledesign.com>
2013-11-24 14:56:28 -0700

    add boilerplate config to svgmin.js

--- tasks/options/svgmin.js
+++ tasks/options/svgmin.js
@@ -1,3 +1,14 @@
 module.exports = {
-
+  svgmin: {                                       // Task
+    options: {                                    // Configuration that will be passed directly to SVGO
+      plugins: [{
+        removeViewBox: false
+      }]
+    },
+    dist: {                                       // Target
+      files: {                                    // Dictionary of files
+        'dist/figure.svg': 'app/figure.svg'       // 'destination': 'source'
+      }
+    }
+  }
 };
