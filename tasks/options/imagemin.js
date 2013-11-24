07c2e585f6dec098dcfe0a1bdfa429099fd68885
kwaledesign <sam@kwaledesign.com>
2013-11-24 11:42:29 -0700

    add boilderplate config to imagemin.js

--- tasks/options/imagemin.js
+++ tasks/options/imagemin.js
@@ -1,3 +1,22 @@
 module.exports = {
-
+  imagemin: {                          // Task
+    static: {                          // Target
+      options: {                       // Target options
+        optimizationLevel: 3
+      },
+      files: {                         // Dictionary of files
+        'dist/img.png': 'src/img.png', // 'destination': 'source'
+        'dist/img.jpg': 'src/img.jpg',
+        'dist/img.gif': 'src/img.gif'
+      }
+    },
+    dynamic: {                         // Another target
+      files: [{
+        expand: true,                  // Enable dynamic expansion
+        cwd: 'src/',                   // Src matches are relative to this path
+        src: ['**/*.{png,jpg,gif}'],   // Actual patterns to match
+        dest: 'dist/'                  // Destination path prefix
+      }]
+    }
+  }
 };
