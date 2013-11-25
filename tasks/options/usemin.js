0900ae66957439858a6a4f6bdafc11ea09b163d6
kwaledesign <sam@kwaledesign.com>
2013-11-24 17:34:11 -0700

    add boilerplate config to usemin.js

--- tasks/options/usemin.js
+++ tasks/options/usemin.js
@@ -1,3 +1,21 @@
 module.exports = {
 
+  // UseminPrepare will only scan a single page for usemin blocks. If you
+  // use usemin blocks that aren't in index.html, create a usemin manifest
+  // page (hackery!) and point this task there.
+  useminPrepare: {
+    options: {
+      dest: '<%%= yeoman.dist %>'
+    },
+    html: '<%%= yeoman.dist %>/index.html'
+  },
+  usemin: {
+    options: {
+        basedir: '<%%= yeoman.dist %>',
+        dirs: ['<%%= yeoman.dist %>/**/*']
+    },
+    html: ['<%%= yeoman.dist %>/**/*.html'],
+    css: ['<%%= yeoman.dist %>/<%= cssDir %>/**/*.css']
+  },
+
 };
